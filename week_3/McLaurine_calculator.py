d4b88d6c1ad4208885eee905ebe25657e8c84642
Tactenator <trevor.mclaurine@gmail.com>
2023-06-05 21:17:17 -0500

    added comments

--- week_3/McLaurine_calculator.py
+++ week_3/McLaurine_calculator.py
@@ -1,3 +1,4 @@
+#initializing functions for add, subtract, divide, and multiply
 def add(x, y): 
     return (x + y);
 
@@ -10,13 +11,17 @@ def divide(x, y):
 def multiply(x, y):
     return (x * y); 
 
+#initializes variables by calling functions.
 newSum = add(8, 1); 
 product = multiply(3, 2);
 difference = subtract(7, 4); 
 quotient = divide(9, 3); 
 
+#Variable used for concatenating a string to display the results of each function.
 output = 'Addition: ' + str(newSum) + '\nSubtraction: ' + str(difference) + '\nMultiplication: ' + str(product) + '\nDivision: ' + str(quotient);
 
+#prints the output variable
 print(output)
 
+#prevents the app from closing. 
 input(" ")
